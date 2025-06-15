65b052a42d9e75db88f4df0aaeb5b163fc6e0585
Mr-Dan-kibet <dan.rotich@student.moringaschool.com>
2025-06-15 11:13:50 +0300

    Allows user to add guests

--- script/script.js
+++ script/script.js
@@ -2,11 +2,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   //Grabbing html elements
   const form = document.getElementById("guestForm");
-  const input = document.getElementById("guestName");
+  const input = document.getElementById("guestNames");
   const List = document.getElementById("guestList");
 
   //stop page from refreshing
   form.addEventListener("submit", (event) => {
     event.preventDefault();
+
+    //Get guest name with trim to remove spaces
+    const name = input.value.trim();
   });
 });
