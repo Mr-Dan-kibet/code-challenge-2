3e58c19a0630e9ba02bf314b3f9c27ebde879cfd
Mr-Dan-kibet <dan.rotich@student.moringaschool.com>
2025-06-15 11:47:43 +0300

    limit guest to 10

--- script/script.js
+++ script/script.js
@@ -11,5 +11,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
     //Get guest name with trim to remove spaces
     const name = input.value.trim();
+
+    //limit list to 10
+    if (List.children.length > 10) {
+      alert("Guest list is full!");
+      return;
+    }
+
+    //create li to put guest names inside
+
+    const li = document.createElement("li");
+    li.textContent = name;
+    // add li to the ul
+    List.appendChild(li);
+    //clear input box
+    input.value = "";
   });
 });
